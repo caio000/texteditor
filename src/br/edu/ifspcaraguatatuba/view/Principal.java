531b1cf3cf7b96f087c60e7943663215ba6813b0
Caio <caiof.adriano@hotmail.com>
2016-08-24 11:52:03 -0300

    Nova modificação
    	Agora o contador de palavras atualiza automaticamente conforme o
    usuário digita o texto.
    	É possivel salvar o arquivo que foi aberto ou um novo arquivo.

--- src/br/edu/ifspcaraguatatuba/view/Principal.java
+++ src/br/edu/ifspcaraguatatuba/view/Principal.java
@@ -1,58 +1,103 @@
 package br.edu.ifspcaraguatatuba.view;
 
-
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-import javax.swing.GroupLayout;
-import javax.swing.GroupLayout.Alignment;
-import javax.swing.JButton;
 import javax.swing.JDialog;
-import javax.swing.JEditorPane;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.Font;
 
-public class Principal extends JFrame {
+public class Teste extends JFrame {
 
-	private static final long serialVersionUID = -4547737506163142377L;
+	private static final long serialVersionUID = 7502264362257727351L;
 	private JPanel contentPane;
-	private JTextField txtCaminho;
-	JMenuBar menuBar;
-	JMenu mnArquivo;
-	private JTextField txtNomeArquivo;
-	private JTextField txtCaminhoArquivo;
-	private JTextField txtQtdPalavras;
-	//private JEditorPane textArea;
+	private JTextField txtPathFile;
 	private JTextArea textArea;
-	private JTextField txtTamanhoAquivo;
 	
-	private String originalText;
+	private String originalText = " ";
+	private JTextField txtFilename;
+	
+	private JMenuItem mntmSalvar;
+	private JTextField txtPath;
+	private JTextField txtFileSize;
+	
+	private JLabel lblQtdPalavras;
 
-	public Principal() {
+	public Teste() {
+		setTitle("Editor de texto");
 		initComponent();
 	}
 	
 	
-	private boolean checkText (String text) {
-		return originalText.equals(text);
+	
+	private void checkFile () {
+		String text = textArea.getText();
+		 
+		 if (!originalText.equals(text)) {
+			 String message = "Você fez modificações no arquivo deseja salvar o arquivo?";
+			 int option = JOptionPane.showConfirmDialog(contentPane, message);
+			 
+			 if (option == 0)
+				 salvarArquivo();
+		 }
+	}
+	
+	private void reset () {
+		txtFilename.setText("");
+		txtPathFile.setText("");
+		textArea.setText("");
+	}
+	
+	private String qtdPalavras (String text) {
+		String qtd = null;
+		
+		String[] palavras = text.split(" "); // Gera um array de palavras
+		qtd = Integer.toString(palavras.length) + " palavra(s)";
+		
+		return qtd;
+	}
+	
+	private void salvarArquivo () {
+		
+		String texto = textArea.getText();
+		if (!originalText.equals(texto)) {
+			try {
+				FileWriter fw = new FileWriter(txtPathFile.getText());
+				PrintWriter writer = new PrintWriter(fw);
+				
+				writer.println(texto);
+				
+				writer.close();
+				
+				mntmSalvar.setEnabled(false);
+				originalText = textArea.getText();
+				JOptionPane.showMessageDialog(contentPane, "Arquivo foi salvo com sucesso!");
+			}catch (Exception e) {
+				JOptionPane.showMessageDialog(contentPane, e.getMessage());
+			}
+		}
 	}
 	
 	private void selecionarArquivo () {
@@ -83,11 +128,13 @@ public class Principal extends JFrame {
 				
 				originalText = temp;
 				textArea.setText(temp);
-				txtCaminho.setText(selectedFile.getAbsolutePath());
-				txtCaminhoArquivo.setText(selectedFile.getAbsolutePath());
-				txtNomeArquivo.setText(selectedFile.getName());
+				txtPathFile.setText(selectedFile.getAbsolutePath());
+				txtFilename.setText(selectedFile.getName());
+				txtPath.setText(selectedFile.getAbsolutePath());
+				double size = selectedFile.length() / 1000;
+				txtFileSize.setText(Double.toString(size));
 				
-				long tamanhoArquivo = selectedFile.length();
+				lblQtdPalavras.setText(qtdPalavras(temp));
 				// TODO Verificar tamnho do arquivo e sua unidade de tamnho como Kb, Mb, Gb .....
 				
 				arq.close();
@@ -98,177 +145,180 @@ public class Principal extends JFrame {
 		
 	}
 	
-	private void initComponent () {
+	
+	
+	public void initComponent () {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 800, 550);
+		setBounds(100, 100, 750, 600);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
 		
-		menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		//-------------------------------------------------MENU----------------------------------------------------------
 		
-		mnArquivo = new JMenu("Arquivo");
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 784, 21);
+		contentPane.add(menuBar);
+		
+		JMenu mnArquivo = new JMenu("Arquivo");
 		menuBar.add(mnArquivo);
 		
-		JMenuItem mntmNovo = new JMenuItem("Novo");
-		mnArquivo.add(mntmNovo);
-		
 		JMenuItem mntmAbrir = new JMenuItem("Abrir");
-		mntmAbrir.addMouseListener(new MouseAdapter() {
+		mntmAbrir.addMouseListener(new MouseAdapter() { // Opção Abrir do menu
 			@Override
-			public void mousePressed(MouseEvent e) { // click da opção Arquivo -> abrir
-				
+			public void mousePressed(MouseEvent e) {
 				selecionarArquivo();
-				
 			}
 		});
 		mnArquivo.add(mntmAbrir);
 		
-		JMenuItem mntmSalvar = new JMenuItem("Salvar");
+		JMenuItem mntmNovo = new JMenuItem("Novo");
+		mntmNovo.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				checkFile();
+				reset();
+			}
+		});
+		mnArquivo.add(mntmNovo);
+		
+		mntmSalvar = new JMenuItem("Salvar");
+		mntmSalvar.addMouseListener(new MouseAdapter() { // Opção Salvar do menu
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				
+				if (txtPathFile.getText().isEmpty()) {
+					System.out.println("Esta vazio");
+					
+					JFileChooser fc = new JFileChooser();
+					
+					fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+					
+					int result = fc.showSaveDialog(contentPane);
+					
+					if (result == JFileChooser.APPROVE_OPTION) {
+						String path = fc.getSelectedFile().getAbsolutePath() + "\\Teste.txt";
+						System.out.println(path);
+						
+						try {
+							FileWriter fw = new FileWriter(path);
+							PrintWriter writer = new PrintWriter(fw);
+							
+							writer.print(textArea.getText());
+							
+							writer.close();
+							
+							originalText = textArea.getText();
+							JOptionPane.showMessageDialog(contentPane, "Arquivo salvo com sucesso!");
+							
+							reset();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
+				} else
+					salvarArquivo();
+			}
+		});
 		mntmSalvar.setEnabled(false);
 		mnArquivo.add(mntmSalvar);
 		
 		JMenu mnAjuda = new JMenu("Ajuda");
-		mnAjuda.addMouseListener(new MouseAdapter() {
+		mnAjuda.addMouseListener(new MouseAdapter() { // Obção ajuda do menu
 			@Override
-			public void mouseClicked(MouseEvent e) {
-				JDialog m = new Modal();
-				m.setVisible(true);
+			public void mousePressed(MouseEvent e) {
+				JDialog ajuda = new Modal();
+				ajuda.setLocationRelativeTo(contentPane);
+				ajuda.setVisible(true);
 			}
 		});
 		menuBar.add(mnAjuda);
 		
 		JMenu mnSair = new JMenu("Sair");
-		mnSair.addMouseListener(new MouseAdapter() {
+		mnSair.addMouseListener(new MouseAdapter() { // Opção sair do menu
 			@Override
 			public void mouseClicked(MouseEvent arg0) {
-				/*
-				 * TODO verificar se houve alteração no texto
-				 * Caso positivo exibir um alert informando o usuário que as modificações serão perdidas.
-				 */
-				
-				int option = JOptionPane.showConfirmDialog(contentPane, "Você não salvou as alterações do arquivo, caso feche o programa as alterações serão perdidas.");
-				System.out.println(option);
+				checkFile(); 
+				dispose();
 			}
 		});
 		menuBar.add(mnSair);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
 		
-		txtCaminho = new JTextField();
-		txtCaminho.setEditable(false);
-		txtCaminho.setColumns(10);
+		//-------------------------------------------------MENU----------------------------------------------------------
 		
-		JButton button = new JButton("...");
-		button.addActionListener(new ActionListener() {
+		textArea = new JTextArea(" ");
+		textArea.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				mntmSalvar.setEnabled(true); // ativa o botão Salvar.
+			}
+		});
+		JScrollPane scrollPane = new JScrollPane(textArea);
+		textArea.setLineWrap(true);
+		scrollPane.setBounds(10, 100, 700, 300);
+		contentPane.add(scrollPane);
+		
+		JLabel lblAbsolutPaht = new JLabel("Caminho:");
+		lblAbsolutPaht.setBounds(10, 72, 81, 14);
+		contentPane.add(lblAbsolutPaht);
+		
+		txtPathFile = new JTextField();
+		txtPathFile.setBounds(65, 69, 546, 20);
+		txtPathFile.setEditable(false);
+		contentPane.add(txtPathFile);
+		txtPathFile.setColumns(10);
+		
+		JButton btnOpenFile = new JButton("...");
+		btnOpenFile.addActionListener(new ActionListener() { // Botão abrir arquivo
 			public void actionPerformed(ActionEvent arg0) {
 				selecionarArquivo();
 			}
 		});
+		btnOpenFile.setBounds(621, 68, 89, 23);
+		contentPane.add(btnOpenFile);
 		
-		JLabel lblArquivo = new JLabel("Arquivo");
-		lblArquivo.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		JLabel lblNomeDoArquivo = new JLabel("Nome do arquivo:");
+		lblNomeDoArquivo.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNomeDoArquivo.setBounds(10, 420, 131, 21);
+		contentPane.add(lblNomeDoArquivo);
 		
-		textArea = new JTextArea();
-		textArea.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent arg0) {
-				
-				// habilitar o botão salvar
-				mntmSalvar.setEnabled(true);
-			}
-		});
+		txtFilename = new JTextField();
+		txtFilename.setBounds(214, 422, 496, 20);
+		contentPane.add(txtFilename);
+		txtFilename.setColumns(10);
 		
-		JLabel lblQuantidadeDePalavras = new JLabel("Quantidade de palavras");
-		lblQuantidadeDePalavras.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtPath = new JTextField();
+		txtPath.setBounds(214, 453, 496, 20);
+		contentPane.add(txtPath);
+		txtPath.setColumns(10);
 		
-		JLabel lblCaminho = new JLabel("Caminho");
-		lblCaminho.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		JLabel lblCaminho = new JLabel("Caminho:");
+		lblCaminho.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblCaminho.setBounds(10, 458, 111, 14);
+		contentPane.add(lblCaminho);
 		
-		JLabel lblNomeDoArquivo = new JLabel("Nome do arquivo");
-		lblNomeDoArquivo.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		txtFileSize = new JTextField();
+		txtFileSize.setEditable(false);
+		txtFileSize.setBounds(214, 484, 73, 20);
+		contentPane.add(txtFileSize);
+		txtFileSize.setColumns(10);
 		
-		txtNomeArquivo = new JTextField();
-		txtNomeArquivo.setColumns(10);
+		JLabel lblFileSize = new JLabel("Tamanho do arquivo:");
+		lblFileSize.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblFileSize.setBounds(10, 486, 142, 14);
+		contentPane.add(lblFileSize);
 		
-		txtCaminhoArquivo = new JTextField();
-		txtCaminhoArquivo.setColumns(10);
+		JLabel lblSizeFile = new JLabel("");
+		lblSizeFile.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblSizeFile.setBounds(294, 490, 46, 14);
+		contentPane.add(lblSizeFile);
 		
-		txtQtdPalavras = new JTextField();
-		txtQtdPalavras.setColumns(10);
+		lblQtdPalavras = new JLabel("");
+		lblQtdPalavras.setBounds(326, 536, 246, 14);
+		contentPane.add(lblQtdPalavras);
 		
-		JLabel lblTamanho = new JLabel("Tamanho");
-		lblTamanho.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		
-		txtTamanhoAquivo = new JTextField();
-		txtTamanhoAquivo.setColumns(10);
-		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(62)
-							.addComponent(lblArquivo)
-							.addGap(18)
-							.addComponent(txtCaminho, GroupLayout.PREFERRED_SIZE, 450, GroupLayout.PREFERRED_SIZE)
-							.addGap(18)
-							.addComponent(button))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addContainerGap(10, Short.MAX_VALUE)
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-										.addGroup(gl_contentPane.createSequentialGroup()
-											.addPreferredGap(ComponentPlacement.RELATED)
-											.addComponent(lblQuantidadeDePalavras))
-										.addGroup(gl_contentPane.createSequentialGroup()
-											.addGap(50)
-											.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-												.addComponent(lblTamanho)
-												.addComponent(lblCaminho))))
-									.addGap(18))
-								.addGroup(gl_contentPane.createSequentialGroup()
-									.addComponent(lblNomeDoArquivo)
-									.addGap(27)))
-							.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-								.addComponent(txtTamanhoAquivo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(txtQtdPalavras, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-								.addComponent(txtNomeArquivo, GroupLayout.DEFAULT_SIZE, 565, Short.MAX_VALUE)
-								.addComponent(txtCaminhoArquivo)))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(22)
-							.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 738, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(14, Short.MAX_VALUE))
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING, false)
-						.addComponent(button, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-							.addComponent(txtCaminho, GroupLayout.DEFAULT_SIZE, 37, Short.MAX_VALUE)
-							.addComponent(lblArquivo, GroupLayout.PREFERRED_SIZE, 31, GroupLayout.PREFERRED_SIZE)))
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(textArea, GroupLayout.PREFERRED_SIZE, 253, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(txtNomeArquivo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(lblNomeDoArquivo))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblTamanho)
-						.addComponent(txtTamanhoAquivo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblCaminho)
-						.addComponent(txtCaminhoArquivo, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblQuantidadeDePalavras)
-						.addComponent(txtQtdPalavras, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
-					.addGap(43))
-		);
-		contentPane.setLayout(gl_contentPane);
 	}
 }
